30ef56eae36d0aacb193d9776ff9a52ebd6f9bb9
sue <sue.song@mail.utoronto.ca>
2020-09-19 14:11:09 -0400

    fixed the unicodes to troubleshoot

--- R/hbd_joel.R
+++ R/hbd_joel.R
@@ -1,11 +1,19 @@
 #' @description turning texts into utf8 code to be printed as emojis.
 emoji_transform = function(text) {
 
-  stringr::str_replace_all(text, c("heart" = "\xE2\x9D\xA4",
-                                   "celebration" = "\xF0\x9F\x8E\x89",
-                                   "tada" = "\xF0\x9F\x8D\xBB",
-                                   "wave" = "\xF0\x9F\x8C\x8A")) %>%
-    utf8::utf8_encode()
+  # stringr::str_replace_all(text, c("heart" = "\xE2\x9D\xA4",
+  #                                  "celebration" = "\xF0\x9F\x8E\x89",
+  #                                  "tada" = "\xF0\x9F\x8D\xBB",
+  #                                  "wave" = "\xF0\x9F\x8C\x8A")) %>%
+  #   utf8::utf8_encode()
+
+  # use unicode instead
+  stringr::str_replace_all(text, c("heart" = "\U1F496",
+                                   "celebration" = "\U1F389",
+                                   "tada" = "\U1F37B",
+                                   "wave" = "\U1F30A")) %>%
+    utf8::utf8_print(quote = FALSE)
+
 }
 
 #' @description this function prints out birthday messages for Joel Le Forestier for his 26th birthday.
