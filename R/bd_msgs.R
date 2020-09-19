f70f8d1fddf7ce30c46c1a92201fcd84053e9199
sue <sue.song@mail.utoronto.ca>
2020-09-19 14:00:31 -0400

    maria msg fixed

--- R/bd_msgs.R
+++ R/bd_msgs.R
@@ -4,7 +4,7 @@ data = tibble::tribble(
   "shelly","Have a great and wonderful birthday, Joel! You rock!",
   "will", "BIRTHDAY BIRTHDAY BIRTHDAY BIRTHDAY HAPPY YAYAYAYA",
   "sue",  "Joel, a SSHRC Scholar, a (soon-to-be) father of Bon Bon, a connoisseur of Whiskey Sour, a solar-powered human emitting positive energy and so fucking much life, and the best labmate and friend anyone can ask for. I am grateful I have you in my life, and thank you for existing. Happy birthday heart",
-  "louisa",  "HAPPY BIRTHDAY JOEL!! celebration \nMay your day of birth be full of dessert and happiness and hopefully we can all celebrate in person together soon! Congratulations on another trip around the sun!!!",
+  "louisa",  "HAPPY BIRTHDAY JOEL!! celebration May your day of birth be full of dessert and happiness and hopefully we can all celebrate in person together soon! Congratulations on another trip around the sun!!!",
   "becca","Happy birthday, Joel! May your day be filled with many donuts or other things that are at least as good as donuts!!",
   "alison","Happy Birthday, Joel!  How fitting for an R whiz to receive his birthday greetings this way.  I hope you have a lovely day!",
   "maria", "Happy Birthday, Joel! I hope that you have a FANTASTIC year that matches just how FANTASTIC you are! I hope that this year brings you love, fun, happiness, adventure, success, and health! You deserve to HAVE IT ALL! Hope you have a BLAST celebrating your special day! Cheers to you! Maria {utf8::utf8_print(intToUtf8(0x1f600))}",
