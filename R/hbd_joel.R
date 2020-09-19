eb7fd6dabb1dab067e33d7ecc948a10d65a29c70
sue <sue.song@mail.utoronto.ca>
2020-09-19 15:46:29 -0400

    corn warning and doesnt' love

--- R/hbd_joel.R
+++ R/hbd_joel.R
@@ -6,22 +6,43 @@
 hbd_joel <- function(from_who) {
 
   # change the name to lower cases if capitalized
-  from_who = tolower(from_who)
 
+  from_who = tolower(from_who)
+  if(any( stringr::str_detect(from_who, "corn") == TRUE)) cat("SHE'S NOT FROM IOWA\n")
+
+  # from_who = ifelse(from_who == "everyone",  hbdjoel::data$name,
+  #                   ifelse(from_who == "anyone", sample(hbdjoel::data$name, 1),
+  #                          ifelse(from_who %in% hbdjoel::data$name, from_who,
+  #                                 ifelse(stringr::str_detect(from_who, "corn") == TRUE, "bethany", from_who))))
+
+  # } else if (stringr::str_detect(from_who, "corn")) {
+  #   warning("SHE'S NOT FROM IOWA")
+  #   from_who = ifelse(str_detect(from_who, "^corn") == TRUE, "bethany", from_who)
+  # } else {
+  #   stop("THIS PERSON DOESN'T LOVE YOU, or you don't know how to type", call. = FALSE)
+  # }
   suppressWarnings(
-    if(from_who == "everyone" ) {
+    if( length(from_who) == 1 & from_who == "everyone" ) {
       from_who = hbdjoel::data$name
 
-    } else if ( from_who == "anyone" ) {
+    } else if ( length(from_who) == 1 & from_who == "anyone" ) {
       from_who = sample(hbdjoel::data$name, 1)
 
     } else if ( from_who %in% hbdjoel::data$name ){
-      from_who = tolower(from_who)
+      from_who = from_who
 
-    } else {
+    } else if ( stringr::str_detect(from_who, "corn")) {
+      from_who = ifelse(str_detect(from_who, "^corn") == TRUE, "bethany", from_who)
+
+    } else if ( length(from_who) == 1 ) {
       stop("THIS PERSON DOESN'T LOVE YOU, or you don't know how to type", call. = FALSE)
       # glue::glue("{from_who} DOESN'T LOVE YOU")
-    })
+    } else {
+      if(any( from_who %in% hbdjoel::data$name == FALSE)) cat(paste(toupper(from_who[which(from_who %in% hbdjoel::data$name == FALSE)]), "DOESN'T LOVE YOU\n"))
+
+      from_who = from_who[which(from_who %in% hbdjoel::data$name)]
+    }
+  )
 
   hbdjoel::data %>%
     dplyr::filter(name %in% from_who) %>%
