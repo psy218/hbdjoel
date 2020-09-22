fc178d92e6b42f49f27eee428c2cca3a7e23a4ed
sue <sue.song@mail.utoronto.ca>
2020-09-22 16:29:12 -0400

    anonymized data saved as extdata

--- R/bd_msgs.R
+++ R/bd_msgs.R
@@ -7,7 +7,7 @@ data = tibble::tribble(
   "louisa",  "HAPPY BIRTHDAY JOEL!! \U1F389 May your day of birth be full of dessert and happiness and hopefully we can all celebrate in person together soon! Congratulations on another trip around the sun!!!",
   "becca","Happy birthday, Joel! May your day be filled with many donuts or other things that are at least as good as donuts!!",
   "alison","Happy Birthday, Joel!  How fitting for an R whiz to receive his birthday greetings this way.  I hope you have a lovely day!",
-  "maria", "Happy Birthday, Joel! I hope that you have a FANTASTIC year that matches just how FANTASTIC you are! I hope that this year brings you love, fun, happiness, adventure, success, and health! You deserve to HAVE IT ALL! Hope you have a BLAST celebrating your special day! Cheers to you! Maria \U1F603",
+  "maria", "Happy Birthday, Joel! I hope that you have a FANTASTIC year that matches just how FANTASTIC you are! I hope that this year brings you love, fun, happiness, adventure, success, and health! You deserve to HAVE IT ALL! Hope you have a BLAST celebrating your special day! Cheers to you! \U1F603",
   "veronica","Here's to you having an AMAZING day and continuing to EXCEL in everything that you do!",
   "jordana","HAPPY BIRTHDAY JOEL THE THIRD!!! Writing a birthday message in an R function is the most Joel thing I’ve ever seen but I am here for it. I hope you have a fabulous birthday!!",
   "bethany","HAPPY BIRTHDAY, JOEL!!! I hope you have a WONDERFUL birthday and are able to spend it doing something ICONIC to properly reflect your TIER-ONE AMAZINGNESS! Thank you for being so wonderful, so extra, so shady, so brilliant, and such a good friend. HAPPY BIRTHDAY! xoxo the corn queen",
